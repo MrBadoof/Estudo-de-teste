772be9ba0cfea356d148db816188a8d9728778fd
Jonas Correa <jonas.santos.correa1@gmail.com>
2024-06-06 17:38:06 -0300

    Atualização do cypress e novo teste

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -1,22 +1,26 @@
 /// <reference types="cypress" />
 
-describe("example to-do app", () => {
+describe('example to-do app', () => {
   beforeEach(() => {
-    cy.visit(
-      "https://cypress-playground.s3.eu-central-1.amazonaws.com/index.html",
-    );
+    cy.visit('/index.html');
   });
 
-  it("test", () => {
+  it('test', () => {
     cy.get('button[type="submit"]').click();
 
-    cy.get("#success")
+    cy.get('#success')
       .should(
-        "contain",
-        "You've been successfully subscribed to our newsletter.",
+        'contain',
+        "You've been successfully subscribed to our newsletter."
       )
-      .and("be.visible");
+      .and('be.visible');
   });
 
-  it("", () => {});
+  it.only('test-2, realizar download de um arquivo e verificar seu conteudo', () => {
+    cy.contains('a', 'Download a text file').click();
+    cy.readFile('cypress/downloads/example.txt').should(
+      'be.equal',
+      'Hello, World!'
+    );
+  });
 });
