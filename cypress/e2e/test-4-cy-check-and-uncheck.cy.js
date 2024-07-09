7da6e1f3b430a9a9239336c7b76925472861d4cd
Jonas Correa <jonas.santos.correa1@gmail.com>
2024-07-09 09:39:36 -0300

    Melhoria no test-4 e novo teste com radio button

--- cypress/e2e/test-4-cy-check-and-uncheck.cy.js
+++ cypress/e2e/test-4-cy-check-and-uncheck.cy.js
@@ -5,17 +5,30 @@ describe('example to-do app', () => {
 
   beforeEach(() => {
     cy.visit('/index.html');
-
-    cy.get('#signature-textarea-with-checkbox').type(text);
-    cy.get('#signature-checkbox').check();
+  });
+  context('Utilizando checkbox', () => {
+    beforeEach(() => {
+      cy.get('#signature-textarea-with-checkbox').type(text);
+      cy.get('#signature-checkbox').check();
+    });
+    it('Escrever texto em campo, clicar em um checkbox e verificar se ele foi destacado logo embaixo', () => {
+      cy.contains('em', text).should('be.visible');
+    });
+    it('Escrever texto em campo, clicar em um checkbox, verificar se ele foi destacado logo embaixo e ao realizar o uncheck o texto some', () => {
+      cy.contains('em', text).should('be.visible');
+      cy.get('#signature-checkbox').uncheck();
+      cy.contains('em', text).should('not.exist');
+    });
   });
 
-  it('Escrever texto em campo, clicar em um checkbox e verificar se ele foi destacado logo embaixo', () => {
-    cy.contains('em', text).should('be.visible');
-  });
-  it('Escrever texto em campo, clicar em um checkbox, verificar se ele foi destacado logo embaixo e ao realizar o uncheck o texto some', () => {
-    cy.contains('em', text).should('be.visible');
-    cy.get('#signature-checkbox').uncheck();
-    cy.contains('em', text).should('not.exist');
+  context('Utilizando radio', () => {
+    it('Seleciona a opção "on" do radio', () => {
+      cy.get('#on').check();
+      cy.contains('p', 'ON').should('be.visible');
+    });
+    it('Seleciona a opção "off" do radio', () => {
+      cy.get('#off').check();
+      cy.contains('p', 'OFF').should('be.visible');
+    });
   });
 });
